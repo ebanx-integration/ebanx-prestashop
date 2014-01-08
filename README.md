1e4042ef2d7f95817901a88f3e199920fa84ba56
Gustavo Henrique Mascarenhas Machado <guhemama@gmail.com>
2014-01-08 15:45:53 -0200

    Updated file headings, added license

---
+++ README.md
@@ -0,0 +1,9 @@
+# EBANX Payment Gateway PrestaShop Extension
+
+This plugin allows you to integrate your PrestaShop store with the EBANX payment gateway.
+It includes support to installments and custom interest rates.
+
+## Installation
+
+## Changelog
+1.0.0: first release
--- modules/ebanx/ebanx.php
+++ modules/ebanx/ebanx.php
@@ -1,26 +1,33 @@
 <?php
 
-/*
- * 2007-2013 PrestaShop
+/**
+ * Copyright (c) 2013, EBANX Tecnologia da Informação Ltda.
+ *  All rights reserved.
  *
- * NOTICE OF LICENSE
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
  *
- * This source file is subject to the Academic Free License (AFL 3.0)
- * that is bundled with this package in the file LICENSE.txt.
- * It is also available through the world-wide-web at this URL:
- * http://opensource.org/licenses/afl-3.0.php
- * If you did not receive a copy of the license and are unable to
- * obtain it through the world-wide-web, please send an email
- * to license@prestashop.com so we can send you a copy immediately.
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
  *
- * DISCLAIMER
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
  *
- * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
- * versions in the future. If you wish to customize PrestaShop for your
- * needs please refer to http://www.prestashop.com for more information.
+ * Neither the name of EBANX nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
  *
- *  @author Ebanx
- *  @copyright  Ebanx 2013
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 if (!defined('_PS_VERSION_'))
@@ -28,6 +35,9 @@ if (!defined('_PS_VERSION_'))
     exit();
 }
 
+/**
+ * The payment module class
+ */
 class Ebanx extends PaymentModule
 {
     public function __construct()
@@ -98,6 +108,7 @@ class Ebanx extends PaymentModule
             Shop::setContext(Shop::CONTEXT_ALL);
         }
 
+        // Create the custom orders table to store the hashes
         if (!$this->_createTables()) {
             return false;
         }
